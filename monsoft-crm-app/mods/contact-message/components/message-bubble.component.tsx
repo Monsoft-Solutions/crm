698ab86221ca01b7bec254591d16171a5495b524
hernan <hernan.fg96x@gmail.com>
2025-06-02 18:04:46 +0200

    message-bubble component

---
+++ monsoft-crm-app/mods/contact-message/components/message-bubble.component.tsx
@@ -0,0 +1,113 @@
+import { forwardRef } from 'react';
+
+import { cn } from '@css/utils';
+
+import { timeToHMEpochStr } from '@shared/utils/tmp';
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@shared/ui/tooltip.ui';
+
+import {
+    communicationChannelTypeToIcon,
+    communicationChannelTypeToTooltip,
+} from '@mods/contact-channel/utils';
+
+import { MessageBubbleProps } from '../schemas';
+
+import { contactMessageStatusToIcon } from '@mods/contact-channel/utils';
+
+export const MessageBubble = forwardRef<HTMLDivElement, MessageBubbleProps>(
+    ({ content, direction, createdAt, channelType, status }, ref) => {
+        const lines = content.split('\n');
+
+        const ChannelIcon = communicationChannelTypeToIcon(channelType);
+
+        const StatusIcon = contactMessageStatusToIcon(status);
+
+        const channelTooltip = communicationChannelTypeToTooltip(channelType);
+
+        const messageBubbleContent = (
+            <div className="w-full p-3 pb-2">
+                <div>
+                    <p className="break-words text-sm leading-relaxed">
+                        {lines.map((line, i) => (
+                            <span key={i}>
+                                {line}
+                                {i < content.length - 1 && <br />}
+                            </span>
+                        ))}
+                    </p>
+                </div>
+
+                <div
+                    ref={ref}
+                    className="mt-1 flex w-full items-center justify-end gap-1.5"
+                >
+                    <>
+                        <p
+                            className={cn(
+                                'text-[10px] font-medium opacity-70',
+                                direction === 'inbound'
+                                    ? 'text-gray-500'
+                                    : 'text-blue-600',
+                            )}
+                        >
+                            {timeToHMEpochStr(createdAt)}
+                        </p>
+
+                        {/* Channel icon with tooltip */}
+                        {
+                            <TooltipProvider>
+                                <Tooltip>
+                                    <TooltipTrigger asChild>
+                                        <div
+                                            className={cn(
+                                                'flex items-center justify-center opacity-60',
+                                                direction === 'inbound'
+                                                    ? 'text-gray-500'
+                                                    : 'text-blue-500',
+                                            )}
+                                        >
+                                            <ChannelIcon />
+                                        </div>
+                                    </TooltipTrigger>
+
+                                    <TooltipContent className="bg-white text-black shadow-md">
+                                        <p className="text-xs">
+                                            {channelTooltip}
+                                        </p>
+                                    </TooltipContent>
+                                </Tooltip>
+                            </TooltipProvider>
+                        }
+
+                        {direction === 'outbound' && (
+                            <span className="opacity-70">
+                                <StatusIcon />
+                            </span>
+                        )}
+                    </>
+                </div>
+            </div>
+        );
+
+        return (
+            <div
+                className={cn(
+                    'relative min-w-[150px] max-w-[85%] overflow-hidden rounded-2xl transition-all',
+                    direction === 'outbound'
+                        ? 'self-end bg-blue-50 shadow-sm hover:shadow'
+                        : 'self-start bg-gray-100 text-gray-800 shadow-sm hover:shadow',
+                )}
+            >
+                {messageBubbleContent}
+            </div>
+        );
+    },
+);
+
+MessageBubble.displayName = 'MessageBubble';
