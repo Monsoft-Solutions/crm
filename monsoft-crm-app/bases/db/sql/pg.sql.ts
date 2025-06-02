b5e8c966f36a8424ff3da9af1624987005c6eaf5
hernan <hernan.fg96x@gmail.com>
2025-06-02 13:54:17 +0200

    bigint-based pg sql timestamp

--- monsoft-crm-app/bases/db/sql/pg.sql.ts
+++ monsoft-crm-app/bases/db/sql/pg.sql.ts
@@ -1,15 +1,15 @@
+import { sql, Writable } from 'drizzle-orm';
+
 export {
     pgTable as table,
     boolean,
     integer as int,
-    bigint,
     char,
     varchar,
     text,
-    timestamp,
 } from 'drizzle-orm/pg-core';
 
-import { Writable } from 'drizzle-orm';
+import { bigint } from 'drizzle-orm/pg-core';
 
 export { pgEnum as enumType } from 'drizzle-orm/pg-core';
 
@@ -31,3 +31,8 @@ type Pool = pg.Pool;
 export { type Pool };
 
 export const dialect = 'postgresql';
+
+export const timestamp = (name: string) =>
+    bigint(name, {
+        mode: 'number',
+    }).default(sql`(UNIX_TIMESTAMP(NOW()) * 1000)`);
