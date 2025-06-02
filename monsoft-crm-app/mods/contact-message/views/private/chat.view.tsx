1c024441c095ac70937dfbb911b51b0b2abb6c2f
hernan <hernan.fg96x@gmail.com>
2025-06-02 18:46:58 +0200

    chat view

---
+++ monsoft-crm-app/mods/contact-message/views/private/chat.view.tsx
@@ -0,0 +1,38 @@
+import { useCallback } from 'react';
+
+import { ChatInterface } from '@mods/contact-message/components';
+
+import { Route } from '@routes/_private/chat';
+
+export function ChatView() {
+    const navigate = Route.useNavigate();
+
+    const { id: activeContactId } = Route.useSearch();
+
+    console.log('-->   ~ ChatView ~ activeContactId:', activeContactId);
+    const setActiveContactId = useCallback(
+        (id: string) => {
+            // If id is empty string, we're clearing the contact selection (going back to list)
+            if (id === '') {
+                void navigate({
+                    search: {}, // Clear all search params including id
+                    replace: true, // Replace current URL to avoid creating extra history entries
+                });
+            } else {
+                void navigate({
+                    search: { id },
+                });
+            }
+        },
+        [navigate],
+    );
+
+    return (
+        <div className="container absolute inset-0 p-0 py-2 sm:py-4 md:py-10">
+            <ChatInterface
+                activeContactId={activeContactId}
+                setActiveContactId={setActiveContactId}
+            />
+        </div>
+    );
+}
