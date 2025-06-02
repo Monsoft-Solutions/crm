53eecab34d9f8d86920d9d43e062d643ce3b22d1
hernan <hernan.fg96x@gmail.com>
2025-06-02 17:32:42 +0200

    send-message-status-to-icon util

---
+++ monsoft-crm-app/mods/contact-channel/utils/contact-message-status-to-icon.util.ts
@@ -0,0 +1,15 @@
+import { ContactMessageStatus } from '@mods/contact-message/enums';
+
+import { Check, CheckCheck, ClockArrowUp } from 'lucide-react';
+
+// Map channel types to their respective icons
+export const contactMessageStatusToIcon = (status: ContactMessageStatus) => {
+    switch (status) {
+        case 'queued':
+            return ClockArrowUp;
+        case 'sent':
+            return Check;
+        case 'delivered':
+            return CheckCheck;
+    }
+};
--- monsoft-crm-app/mods/contact-channel/utils/index.ts
+++ monsoft-crm-app/mods/contact-channel/utils/index.ts
@@ -1,2 +1,3 @@
 export * from './communication-channel-type-to-icon.util';
 export * from './communication-channel-type-to-tooltip.util';
+export * from './contact-message-status-to-icon.util';
