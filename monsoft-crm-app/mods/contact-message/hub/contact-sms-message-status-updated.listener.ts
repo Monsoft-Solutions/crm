15157b8c6f3488128788a613eefd6d0402f0455a
hernan <hernan.fg96x@gmail.com>
2025-06-03 13:18:25 +0200

    contact-sms-message-status-updated listener emit contact-message-status-updated on success

--- monsoft-crm-app/mods/contact-message/hub/contact-sms-message-status-updated.listener.ts
+++ monsoft-crm-app/mods/contact-message/hub/contact-sms-message-status-updated.listener.ts
@@ -1,5 +1,6 @@
 import { catchError } from '@errors/utils/catch-error.util';
 
+import { emit } from '@events/providers';
 import { listen } from '@events/providers/listen.provider';
 
 import { db } from '@db/providers/server';
@@ -18,4 +19,33 @@ void listen('smsStatusUpdated', async ({ sid, status }) => {
     );
 
     if (contactSmsMessageUpdateError) return;
+
+    const { data: contactSmsMessage, error: contactSmsMessageError } =
+        await catchError(
+            db.query.contactSmsMessage.findFirst({
+                where: (record, { eq }) => eq(record.sid, sid),
+                with: {
+                    contactPhoneNumber: {
+                        with: {
+                            contact: true,
+                        },
+                    },
+                },
+            }),
+        );
+
+    if (contactSmsMessageError) return;
+    if (!contactSmsMessage) return;
+
+    const { id, contactPhoneNumber } = contactSmsMessage;
+    const { contactId } = contactPhoneNumber;
+
+    emit({
+        event: 'contactMessageStatusUpdated',
+        payload: {
+            id,
+            contactId,
+            status,
+        },
+    });
 });
