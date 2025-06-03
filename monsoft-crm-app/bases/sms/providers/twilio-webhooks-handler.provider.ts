d8afba1d87f9958bc3eb5a84b44c759c575efa13
hernan <hernan.fg96x@gmail.com>
2025-06-03 14:13:35 +0200

    fix twilio-webhooks-handler provider add twilio-status-updated-webhook-handler

--- monsoft-crm-app/bases/sms/providers/twilio-webhooks-handler.provider.ts
+++ monsoft-crm-app/bases/sms/providers/twilio-webhooks-handler.provider.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
 import { twilioIncomingSmsWebhookHandler } from './twilio-incoming-sms-webhook-handler.provider';
+import { twilioStatusUpdatedWebhookHandler } from './twilio-status-updated-webhook-handler.provider';
 
 export function twilioWebhooksHandler(server: express.Express) {
     twilioIncomingSmsWebhookHandler(server);
+    twilioStatusUpdatedWebhookHandler(server);
 }
