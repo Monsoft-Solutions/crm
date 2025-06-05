bd79b783c2a851711aaae775221c12be97b448bb
hernan <hernan.fg96x@gmail.com>
2025-06-05 17:03:34 +0200

    view-edit-dialog component

---
+++ monsoft-crm-app/shared/components/view-edit-dialog.component.tsx
@@ -0,0 +1,114 @@
+import { HTMLAttributes, useState } from 'react';
+
+import { Button } from '@ui/button.ui';
+import { Switch } from '@ui/switch.ui';
+import { Spinner } from '@ui/spinner.ui';
+
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from '@ui/dialog.ui';
+
+export function ViewEditDialog({
+    title,
+    render,
+    onSubmit,
+    onClose,
+    initEditMode = false,
+    newEntityCreation = false,
+    children,
+}: HTMLAttributes<HTMLDivElement> & {
+    title: string;
+    onSubmit?: () => void | Promise<void>;
+    onClose?: () => void;
+    render: ({
+        isEditMode,
+        setIsSubmitting,
+    }: {
+        isEditMode: boolean;
+        setIsSubmitting: (isSubmitting: boolean) => void;
+    }) => React.ReactNode;
+    initEditMode?: boolean;
+    newEntityCreation?: boolean;
+}) {
+    const [isEditMode, setIsEditMode] = useState(initEditMode);
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const handleSubmit = async () => {
+        setIsSubmitting(true);
+
+        try {
+            await onSubmit?.();
+            setIsSubmitting(false);
+        } catch (error) {
+            console.error(error);
+            setIsSubmitting(false);
+            return;
+        }
+
+        handleOpenChange(false);
+    };
+
+    const handleOpenChange = (open: boolean) => {
+        if (!open) onClose?.();
+        setIsOpen(open);
+    };
+
+    return (
+        <Dialog onOpenChange={handleOpenChange} open={isOpen}>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+
+            <DialogContent className="block max-h-[90vh] w-[90vw] max-w-none overflow-y-auto rounded-lg border-gray-200 p-6 shadow-lg md:w-[80vw] lg:w-[60vw] lg:max-w-[800px] lg:p-6">
+                <DialogHeader className="mb-10">
+                    <DialogTitle className="text-left">{title}</DialogTitle>
+
+                    {!newEntityCreation && (
+                        <div className="absolute right-14 top-2 z-10 flex items-center gap-2">
+                            <span className="text-sm font-medium transition-all duration-300">
+                                {isEditMode ? 'Edit Mode' : 'View Mode'}
+                            </span>
+
+                            <Switch
+                                id="edit-mode-switch"
+                                checked={isEditMode}
+                                onCheckedChange={setIsEditMode}
+                            />
+                        </div>
+                    )}
+                </DialogHeader>
+
+                {render({ isEditMode, setIsSubmitting })}
+
+                {isEditMode && (
+                    <div className="mt-4 flex justify-end space-x-4">
+                        <Button
+                            type="button"
+                            variant="outline"
+                            onClick={() => {
+                                setIsEditMode(false);
+                            }}
+                        >
+                            Cancel
+                        </Button>
+
+                        <Button
+                            type="submit"
+                            disabled={isSubmitting}
+                            onClick={() => {
+                                void handleSubmit();
+                            }}
+                        >
+                            {isSubmitting ? <Spinner /> : 'Save'}
+                        </Button>
+                    </div>
+                )}
+            </DialogContent>
+        </Dialog>
+    );
+}
