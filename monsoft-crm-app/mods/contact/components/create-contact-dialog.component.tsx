5c47eda3f3e3fdbcf9b7e2b6b3ed88900047f9c6
hernan <hernan.fg96x@gmail.com>
2025-06-05 17:11:33 +0200

    create-contact-dialog component

---
+++ monsoft-crm-app/mods/contact/components/create-contact-dialog.component.tsx
@@ -0,0 +1,63 @@
+import { HTMLAttributes } from 'react';
+
+import { toast } from 'sonner';
+
+import { api } from '@api/providers/web';
+
+import { Plus } from 'lucide-react';
+
+import { Button } from '@ui/button.ui';
+
+import { ContactDetailsDialog } from './contact-details-dialog.component';
+
+export function CreateContactDialog({
+    brandId,
+    onClose,
+    children,
+}: HTMLAttributes<HTMLDivElement> & {
+    brandId: string;
+    onClose?: () => void;
+}) {
+    const { mutateAsync: createContact } =
+        api.contact.createContact.useMutation();
+
+    return (
+        <ContactDetailsDialog
+            brandId={brandId}
+            onSubmit={async (data) => {
+                const createContactResult = await createContact(data);
+                const { error } = createContactResult;
+
+                if (error) {
+                    toast.error('Failed to create contact', {
+                        description: 'Please try again.',
+                    });
+                    return;
+                }
+
+                toast('Contact successfully created', {
+                    description: `you can now get in touch with ${data.firstName} !`,
+                });
+
+                onClose?.();
+                return;
+            }}
+            onClose={() => {
+                onClose?.();
+            }}
+            newEntityCreation={true}
+        >
+            {children ? (
+                children
+            ) : (
+                <Button
+                    variant="ghost"
+                    size="sm"
+                    className="h-9 w-9 flex-shrink-0 rounded-full bg-blue-50 p-0 text-blue-600 hover:bg-blue-100"
+                >
+                    <Plus className="h-5 w-5" />
+                </Button>
+            )}
+        </ContactDetailsDialog>
+    );
+}
