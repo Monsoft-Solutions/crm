8c3549e541e9694c199256058dd50a2086f60668
hernan <hernan.fg96x@gmail.com>
2025-06-10 16:06:56 +0200

    ai core conf: languse (base-url, public/private-key) & anthropic (api-key)

--- monsoft-crm-app/bases/conf/core/seed/constants/core-config-data.constant.ts
+++ monsoft-crm-app/bases/conf/core/seed/constants/core-config-data.constant.ts
@@ -14,4 +14,10 @@ export const coreConfigData: InferInsertModel<typeof coreConfTable> = {
     twilioSid: z.string().parse(process.env.MSS_TWILIO_SID),
     twilioToken: z.string().parse(process.env.MSS_TWILIO_TOKEN),
     twilioFrom: z.string().parse(process.env.MSS_TWILIO_FROM),
+
+    langfuseBaseUrl: z.string().parse(process.env.LANGFUSE_BASE_URL),
+    langfusePublicKey: z.string().parse(process.env.LANGFUSE_PUBLIC_KEY),
+    langfuseSecretKey: z.string().parse(process.env.LANGFUSE_SECRET_KEY),
+
+    anthropicApiKey: z.string().parse(process.env.ANTHROPIC_API_KEY),
 };
