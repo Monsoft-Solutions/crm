bf31cb259d1c3a5838ba4fa31cace5b0b05e650c
hernan <hernan.fg96x@gmail.com>
2025-06-10 16:21:40 +0200

    generate-text provider

---
+++ monsoft-crm-app/bases/ai/providers/generate-text.provider.ts
@@ -0,0 +1,49 @@
+import { Function } from '@errors/types';
+import { Error, Success } from '@errors/utils';
+
+import { Message } from 'ai';
+
+import { streamText } from './stream-text.provider';
+
+import { AiRequest } from '../schemas/ai-request.schema';
+
+export const generateText = (async ({
+    prompt,
+    messages,
+    modelParams,
+}: (
+    | {
+          prompt: string;
+          messages?: undefined;
+      }
+    | {
+          prompt?: undefined;
+          messages: Message[];
+      }
+) & {
+    modelParams: AiRequest;
+}) => {
+    const { data: textStream, error: textGenerationError } = await streamText(
+        messages
+            ? {
+                  messages,
+                  modelParams,
+              }
+            : { prompt, modelParams },
+    );
+
+    if (textGenerationError) return Error();
+
+    let text = '';
+
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    while (true) {
+        const { done, value: textDelta } = await textStream.read();
+
+        if (done) break;
+
+        text += textDelta;
+    }
+
+    return Success(text);
+}) satisfies Function<{ prompt: string; modelParams: AiRequest }, string>;
