080d2d0bda283a6132518f2d56c77361fa08180a
hernan <hernan.fg96x@gmail.com>
2025-06-11 14:39:28 +0200

    remove old twilio incoming-sms/status-update paths, schemas & providers

--- monsoft-crm-app/bases/sms/schemas/twilio-incoming-sms-webhook-body.schema.ts
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod';
-
-export const twilioIncomingSmsWebhookBodySchema = z.object({
-    From: z.string(),
-    Body: z.string(),
-});
--- monsoft-crm-app/bases/sms/schemas/twilio-status-update-webhook-body.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-import { contactMessageStatusEnum } from '@mods/contact-message/enums';
-
-export const twilioStatusUpdateWebhookBodySchema = z.object({
-    MessageSid: z.string(),
-    MessageStatus: contactMessageStatusEnum,
-});
