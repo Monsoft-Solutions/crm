088f5a94359c80f4a1d0775ae927f00798835b7a
hernan <hernan.fg96x@gmail.com>
2025-06-11 14:35:24 +0200

    twilio-event-webhook-handler provider

---
+++ monsoft-crm-app/bases/sms/providers/twilio-event-webhook-handler.provider.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+
+import { twilioEventWebhookPath } from '../constants';
+
+import { twilioEventWebhookBodySchema } from '../schemas';
+
+import { emit } from '@events/providers';
+
+export function twilioEventWebhookHandler(server: express.Express) {
+    server.use(twilioEventWebhookPath, express.json());
+
+    server.post(twilioEventWebhookPath, (req, res) => {
+        console.log(req.body);
+
+        const parsedBody = twilioEventWebhookBodySchema.parse(req.body);
+
+        for (const event of parsedBody) {
+            switch (event.type) {
+                case 'com.twilio.messaging.inbound-message.received': {
+                    const { from, body } = event.data;
+
+                    emit({
+                        event: 'smsReceived',
+                        payload: {
+                            from,
+                            body,
+                            createdAt: Date.now(),
+                        },
+                    });
+
+                    break;
+                }
+
+                case 'com.twilio.messaging.message.sent': {
+                    const { messageSid } = event.data;
+
+                    emit({
+                        event: 'smsStatusUpdated',
+                        payload: {
+                            sid: messageSid,
+                            status: 'sent',
+                        },
+                    });
+
+                    break;
+                }
+
+                case 'com.twilio.messaging.message.delivered': {
+                    const { messageSid } = event.data;
+
+                    emit({
+                        event: 'smsStatusUpdated',
+                        payload: {
+                            sid: messageSid,
+                            status: 'delivered',
+                        },
+                    });
+
+                    break;
+                }
+            }
+        }
+
+        res.send();
+    });
+}
