e2dd80d9c05e1fb1c59c5aac2dcb68fee5108789
hernan <hernan.fg96x@gmail.com>
2025-06-11 14:37:42 +0200

    send-sms provider remove status-callback url (replaced by centralized event-webhook)

--- monsoft-crm-app/bases/sms/providers/send-sms.provider.ts
+++ monsoft-crm-app/bases/sms/providers/send-sms.provider.ts
@@ -4,17 +4,12 @@ import { Function } from '@errors/types';
 import { Error, Success } from '@errors/utils';
 import { catchError } from '@errors/utils/catch-error.util';
 
-import { appUrl } from '@dist/constants';
-
-import { twilioStatusUpdateWebhookPath } from '@sms/constants';
-
 export const sendSms = (async ({ client, from, to, body }) => {
     const { data: message, error: messageError } = await catchError(
         client.messages.create({
             body,
             from,
             to,
-            statusCallback: `${appUrl}${twilioStatusUpdateWebhookPath}`,
         }),
     );
 
