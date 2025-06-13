8049222eae438ef34116d13c72a069acfea92d09
hernan <hernan.fg96x@gmail.com>
2025-06-13 12:59:07 +0200

    create-brand-card component

---
+++ monsoft-crm-app/mods/brand/components/create-brand-card.component.tsx
@@ -0,0 +1,35 @@
+import {
+    Card,
+    CardContent,
+    CardTitle,
+    CardHeader,
+    CardDescription,
+} from '@ui/card.ui';
+
+import { CreateBrandForm } from './create-brand-form.component';
+
+import { api } from '@api/providers/web';
+
+export function CreateBrandCard() {
+    const { data: availablePhoneNumbers } =
+        api.brand.getAvailablePhoneNumbers.useQuery();
+
+    if (!availablePhoneNumbers) return null;
+
+    return (
+        <Card className="container w-full max-w-md self-center">
+            <CardHeader className="mb-4">
+                <CardTitle>Create Brand</CardTitle>
+                <CardDescription>
+                    a business that you manage contacts for.
+                </CardDescription>
+            </CardHeader>
+
+            <CardContent>
+                <CreateBrandForm
+                    availablePhoneNumbers={availablePhoneNumbers}
+                />
+            </CardContent>
+        </Card>
+    );
+}
