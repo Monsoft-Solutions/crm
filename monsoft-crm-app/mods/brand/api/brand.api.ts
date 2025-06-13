139a961dc0792e5fa55c6363f039b180131d64cd
hernan <hernan.fg96x@gmail.com>
2025-06-13 12:35:00 +0200

    get-available-phone-numbers query

--- monsoft-crm-app/mods/brand/api/brand.api.ts
+++ monsoft-crm-app/mods/brand/api/brand.api.ts
@@ -3,6 +3,7 @@ import { endpoints } from '@api/providers/server';
 // queries
 import { getBrand } from './get-brand.query';
 import { getBrandsIds } from './get-brands-ids.query';
+import { getAvailablePhoneNumbers } from './get-available-phone-numbers.query';
 
 // mutations
 import { createBrand } from './create-brand.mutation';
@@ -13,6 +14,7 @@ export const brand = endpoints({
     // queries
     getBrand,
     getBrandsIds,
+    getAvailablePhoneNumbers,
 
     // mutations
     createBrand,
