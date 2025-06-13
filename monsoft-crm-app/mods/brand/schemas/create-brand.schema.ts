e679f14fe0adc9dac62b3e644164f35d9b94abc5
hernan <hernan.fg96x@gmail.com>
2025-06-13 12:40:37 +0200

    create-brand schema add optional phone-number

--- monsoft-crm-app/mods/brand/schemas/create-brand.schema.ts
+++ monsoft-crm-app/mods/brand/schemas/create-brand.schema.ts
@@ -2,6 +2,7 @@ import { z } from 'zod';
 
 export const createBrandSchema = z.object({
     name: z.string(),
+    phoneNumber: z.string().optional(),
 });
 
 export type CreateBrand = z.infer<typeof createBrandSchema>;
