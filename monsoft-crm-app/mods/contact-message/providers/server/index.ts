734396691353efda509ad75cedc40a50dad57d57
hernan <hernan.fg96x@gmail.com>
2025-06-17 12:03:37 +0200

    send-whatsapp-to-contact provider

--- monsoft-crm-app/mods/contact-message/providers/server/index.ts
+++ monsoft-crm-app/mods/contact-message/providers/server/index.ts
@@ -1,5 +1,6 @@
 export * from './send-email-to-contact.provider';
 export * from './send-sms-to-contact-phone-number.provider';
 export * from './send-sms-to-contact.provider';
+export * from './send-whatsapp-to-contact.provider';
 
 export * from './get-contact-messages.provider';
