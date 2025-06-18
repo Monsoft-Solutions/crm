11212fb27f24669a69ed31c0f1c8971bba5d9618
hernan <hernan.fg96x@gmail.com>
2025-06-18 15:11:11 +0200

    send-email-to-contact-email-address provider

---
+++ monsoft-crm-app/mods/contact-message/providers/server/send-email-to-contact-email-address.provider.ts
@@ -0,0 +1,77 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { Function } from '@errors/types';
+import { Error, Success } from '@errors/utils';
+import { catchError } from '@errors/utils/catch-error.util';
+
+import { db } from '@db/providers/server';
+import tables from '@db/db';
+
+import { sendBrandEmail } from '@mods/brand/providers/send-brand-email.provider';
+
+export const sendEmailToContactEmailAddress = (async ({
+    contactEmailAddressId,
+    username,
+    subject,
+    body,
+}) => {
+    const { data: contactEmailAddress, error: contactEmailAddressError } =
+        await catchError(
+            db.query.contactEmailAddress.findFirst({
+                where: (record, { eq }) => eq(record.id, contactEmailAddressId),
+                with: {
+                    contact: {
+                        with: {
+                            brand: true,
+                        },
+                    },
+                },
+            }),
+        );
+
+    if (contactEmailAddressError) return Error();
+
+    if (contactEmailAddress === undefined) return Error();
+
+    const { contact, emailAddress } = contactEmailAddress;
+
+    const { brandId } = contact;
+
+    const { error: sendEmailError } = await sendBrandEmail({
+        brandId,
+        username,
+        to: emailAddress,
+        subject,
+        body,
+    });
+
+    if (sendEmailError) return Error();
+
+    const id = uuidv4();
+
+    const contactEmail = {
+        id,
+        contactEmailAddressId,
+        body,
+    };
+
+    const { error: dbError } = await catchError(
+        db.insert(tables.contactEmail).values(contactEmail),
+    );
+
+    if (dbError) return Error();
+
+    const result = {
+        id,
+    };
+
+    return Success(result);
+}) satisfies Function<
+    {
+        contactEmailAddressId: string;
+        username: string;
+        subject: string;
+        body: string;
+    },
+    { id: string }
+>;
