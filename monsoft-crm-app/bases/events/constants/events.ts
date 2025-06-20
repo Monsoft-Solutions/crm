632c324e56bb9479eca8c84035e719af68a19fa4
hernan <hernan.fg96x@gmail.com>
2025-06-20 13:02:22 +0200

    email-status-updated event

--- monsoft-crm-app/bases/events/constants/events.ts
+++ monsoft-crm-app/bases/events/constants/events.ts
@@ -1,10 +1,12 @@
 // all defined events
 
 import * as smsEvents from '@twilio/events';
+import * as emailEvents from '@email/events';
 
 import * as appEvents from '@app/events';
 
 export const events = {
     ...smsEvents,
+    ...emailEvents,
     ...appEvents,
 };
