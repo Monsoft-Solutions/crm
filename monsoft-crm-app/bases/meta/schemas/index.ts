eb6b06a4ba9686b5ee217004a991e3dfbdf2dcd8
hernan <hernan.fg96x@gmail.com>
2025-07-01 11:59:54 +0200

    meta-event-whatsapp-messages-status-update schema

--- monsoft-crm-app/bases/meta/schemas/index.ts
+++ monsoft-crm-app/bases/meta/schemas/index.ts
@@ -2,3 +2,4 @@ export * from './send-whatsapp-response.schema';
 export * from './meta-event-webhook-body.schema';
 export * from './meta-event-webhook-body-messages-field-change-value.schema';
 export * from './meta-event-new-whatsapp-messages.schema';
+export * from './meta-event-whatsapp-messages-status-update.schema';
