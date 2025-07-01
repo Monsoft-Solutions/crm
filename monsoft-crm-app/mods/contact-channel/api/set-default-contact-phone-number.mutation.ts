99eeb9f984ee5588410bbfedd21a03238b411307
hernan <hernan.fg96x@gmail.com>
2025-07-01 12:32:19 +0200

    fix set-default-contact-phone-number mutation set is-default to null only where contact-id correspond to the owner of the specified contact-phone-number

--- monsoft-crm-app/mods/contact-channel/api/set-default-contact-phone-number.mutation.ts
+++ monsoft-crm-app/mods/contact-channel/api/set-default-contact-phone-number.mutation.ts
@@ -6,7 +6,7 @@ import { catchError } from '@errors/utils/catch-error.util';
 import { protectedEndpoint } from '@api/providers/server';
 import { queryMutationCallback } from '@api/providers/server/query-mutation-callback.provider';
 
-import { eq, ne } from 'drizzle-orm';
+import { and, eq, ne } from 'drizzle-orm';
 import { db } from '@db/providers/server';
 import tables from '@db/db';
 
@@ -16,12 +16,31 @@ export const setDefaultContactPhoneNumber = protectedEndpoint
         queryMutationCallback(async ({ input: { phoneNumberId } }) => {
             const { error } = await catchError(
                 db.transaction(async (tx) => {
+                    const contactPhoneNumber =
+                        await tx.query.contactPhoneNumber.findFirst({
+                            where: (record, { eq }) =>
+                                eq(record.id, phoneNumberId),
+                        });
+
+                    if (!contactPhoneNumber)
+                        throw 'CONTACT_PHONE_NUMBER_NOT_FOUND';
+
+                    const { contactId } = contactPhoneNumber;
+
                     await tx
                         .update(tables.contactPhoneNumber)
                         .set({
                             isDefault: null,
                         })
-                        .where(ne(tables.contactPhoneNumber.id, phoneNumberId));
+                        .where(
+                            and(
+                                eq(
+                                    tables.contactPhoneNumber.contactId,
+                                    contactId,
+                                ),
+                                ne(tables.contactPhoneNumber.id, phoneNumberId),
+                            ),
+                        );
 
                     await tx
                         .update(tables.contactPhoneNumber)
