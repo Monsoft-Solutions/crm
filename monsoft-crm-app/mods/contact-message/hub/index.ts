130dcf226ac1fe726a1016f29502315f7d8ed534
hernan <hernan.fg96x@gmail.com>
2025-07-01 12:11:08 +0200

    contact-whatsapp-message-status-updated listener

--- monsoft-crm-app/mods/contact-message/hub/index.ts
+++ monsoft-crm-app/mods/contact-message/hub/index.ts
@@ -1,3 +1,4 @@
 export * from './contact-sms-message-received.listener';
 export * from './contact-sms-message-status-updated.listener';
 export * from './contact-whatsapp-message-received.listener';
+export * from './contact-whatsapp-message-status-updated.listener';
