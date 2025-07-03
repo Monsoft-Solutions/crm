13391779dc8e85e5069ef1142ca99162b1c9b484
hernan <hernan.fg96x@gmail.com>
2025-07-03 16:01:11 +0200

    query-mutation-callback provider input callback include additional-arg db as drizzle transaction for atomic endpoints

--- monsoft-crm-app/bases/api/providers/server/query-mutation-callback.provider.ts
+++ monsoft-crm-app/bases/api/providers/server/query-mutation-callback.provider.ts
@@ -1,6 +1,22 @@
 import { QueryMutationCallback } from '@api/types/server/query-mutation-callback.type';
 
+import { Success, Error } from '@errors/utils';
+
+import { Tx } from '@db/types';
+
+import { db } from '@db/providers/server';
+
 // utility to create a tRPC query/mutation
 export const queryMutationCallback = <Context, Input, Output>(
-    callback: QueryMutationCallback<Context, Input, Output>,
-) => callback;
+    callback: QueryMutationCallback<Context, Input, Output, { db: Tx }>,
+) => {
+    return (async (args: { ctx: Context; input: Input }) => {
+        const { data, error } = await db.transaction(async (db) => {
+            return callback({ ...args, db });
+        });
+
+        if (error) return Error(error);
+
+        return Success(data);
+    }) as QueryMutationCallback<Context, Input, Output>;
+};
