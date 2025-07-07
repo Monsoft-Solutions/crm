1ac52da0023425e398891af5ced27c82c65ab5e4
hernan <hernan.fg96x@gmail.com>
2025-07-07 14:10:52 +0200

    get-contacts ids query sort by last event using get-contact-last-event provider

--- monsoft-crm-app/mods/contact/api/get-contacts-ids.query.ts
+++ monsoft-crm-app/mods/contact/api/get-contacts-ids.query.ts
@@ -6,6 +6,8 @@ import { catchError } from '@errors/utils/catch-error.util';
 import { protectedEndpoint } from '@api/providers/server';
 import { queryMutationCallback } from '@api/providers/server/query-mutation-callback.provider';
 
+import { getContactLastEvent } from '@mods/contact/providers/get-contact-last-event.provider';
+
 export const getContactsIds = protectedEndpoint
     .input(
         z.object({
@@ -25,6 +27,34 @@ export const getContactsIds = protectedEndpoint
 
             if (error) return Error();
 
-            return Success(contacts);
+            const contactsWithLastEventTimes = await Promise.all(
+                contacts.map(async (contact) => {
+                    const { data: lastEvent, error: lastEventError } =
+                        await getContactLastEvent({
+                            contactId: contact.id,
+                            db,
+                        });
+
+                    if (!lastEventError)
+                        return {
+                            id: contact.id,
+                            timestamp: lastEvent.timestamp,
+                        };
+                }),
+            );
+
+            if (
+                contactsWithLastEventTimes.some(
+                    (contact) => contact === undefined,
+                )
+            )
+                return Error();
+
+            const contactsSorted = contactsWithLastEventTimes
+                .filter((contact) => contact !== undefined)
+                .sort((a, b) => b.timestamp - a.timestamp)
+                .map(({ id }) => ({ id }));
+
+            return Success(contactsSorted);
         }),
     );
