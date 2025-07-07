785d22ce5341edb72ab34ce0d55ab97a8588c99a
hernan <hernan.fg96x@gmail.com>
2025-07-07 15:44:33 +0200

    mark-all-contact-inbound-messages-as-read mutation

---
+++ monsoft-crm-app/mods/contact-message/api/mark-all-contact-inbound-messages-as-read.mutation.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod';
+
+import { Error, Success } from '@errors/utils';
+
+import { protectedEndpoint } from '@api/providers/server';
+import { queryMutationCallback } from '@api/providers/server/query-mutation-callback.provider';
+
+import { markAllContactInboundMessagesAsRead as markAllContactInboundMessagesAsReadProvider } from '../providers/server/mark-all-contact-inbound-messages-as-read.provider';
+
+export const markAllContactInboundMessagesAsRead = protectedEndpoint
+    .input(z.object({ contactId: z.string() }))
+    .mutation(
+        queryMutationCallback(async ({ input: { contactId }, db }) => {
+            const { error } = await markAllContactInboundMessagesAsReadProvider(
+                {
+                    contactId,
+                    db,
+                },
+            );
+
+            if (error) return Error();
+
+            console.log('all messages marked as read for contact', contactId);
+
+            return Success();
+        }),
+    );
