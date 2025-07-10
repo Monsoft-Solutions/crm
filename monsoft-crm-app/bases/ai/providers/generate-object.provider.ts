61cffa6d5216b7feabd7445332b541e77eb46367
hernan <hernan.fg96x@gmail.com>
2025-07-10 14:46:23 +0200

    generate-object, stream-text providers use ai-model-to-sdk util

--- monsoft-crm-app/bases/ai/providers/generate-object.provider.ts
+++ monsoft-crm-app/bases/ai/providers/generate-object.provider.ts
@@ -1,24 +1,17 @@
-import { Function } from '@errors/types';
-import { Error, Success } from '@errors/utils';
-import { catchError } from '@errors/utils/catch-error.util';
-
-import { getCoreConf } from '@conf/providers/server';
-
-import { v4 as uuidv4 } from 'uuid';
-
 import { ZodSchema } from 'zod';
 
 import {
     generateObject as aiSdkGenerateObject,
     GenerateObjectResult,
 } from 'ai';
-import { createAnthropic } from '@ai-sdk/anthropic';
 
-import { Langfuse } from 'langfuse';
+import { Function } from '@errors/types';
+import { Error, Success } from '@errors/utils';
+import { catchError } from '@errors/utils/catch-error.util';
 
 import { AiRequest } from '../schemas';
 
-let langfuse: Langfuse | undefined;
+import { aiModelToSdkModel } from '@ai/utils';
 
 export const generateObject = (async ({
     prompt,
@@ -30,92 +23,21 @@ export const generateObject = (async ({
     modelParams: AiRequest;
     outputSchema: ZodSchema;
 }) => {
-    // get the core configuration
-    const coreConfWithError = await getCoreConf();
+    const { data: model, error: modelError } = await aiModelToSdkModel(
+        modelParams.model,
+    );
 
-    const { error: coreConfError } = coreConfWithError;
-
-    if (coreConfError !== null) return Error('MISSING_CORE_CONF');
-
-    const { data: coreConf } = coreConfWithError;
-
-    const {
-        anthropicApiKey,
-        langfuseSecretKey,
-        langfusePublicKey,
-        langfuseBaseUrl,
-    } = coreConf;
-
-    if (!langfuse) {
-        initLangfuse({
-            langfuseSecretKey,
-            langfusePublicKey,
-            langfuseBaseUrl,
-        });
-    }
-
-    const traceId = uuidv4();
-
-    const trace = langfuse?.trace({
-        name: modelParams.callerName,
-        id: traceId,
-        metadata: {
-            modelParams,
-        },
-        sessionId: modelParams.chatSessionId ?? traceId,
-        userId: modelParams.userId,
-        tags: [modelParams.model, modelParams.provider, modelParams.callerName],
-    });
-
-    if (modelParams.provider !== 'anthropic') {
-        return Error('PROVIDER_NOT_SUPPORTED');
-    }
-
-    const anthropic = createAnthropic({
-        apiKey: anthropicApiKey,
-    });
-
-    const model = modelParams.model;
-
-    const generation = trace?.generation({
-        model,
-        input: prompt,
-        metadata: {
-            userId: modelParams.userId,
-        },
-    });
+    if (modelError) return Error('FAIL_TO_INFER_SDK_MODEL');
 
     const { data: result, error } = await catchError(
         aiSdkGenerateObject({
-            model: anthropic(model),
+            model,
             prompt,
             schema: outputSchema,
         }),
     );
 
-    if (error) {
-        generation?.end({
-            input: prompt,
-            output: `Error: ${String(error)}`,
-            metadata: {
-                error: String(error),
-            },
-        });
-        await langfuse?.flushAsync();
-        return Error('AI_GENERATE_OBJECT_FAILED');
-    }
-
-    generation?.end({
-        input: prompt,
-        output: result.object,
-        usage: {
-            input: result.usage.promptTokens,
-            output: result.usage.completionTokens,
-            total: result.usage.totalTokens,
-        },
-    });
-
-    await langfuse?.flushAsync();
+    if (error) return Error('AI_GENERATE_OBJECT_FAILED');
 
     return Success(result.object);
 }) satisfies Function<
@@ -126,20 +48,3 @@ export const generateObject = (async ({
     },
     GenerateObjectResult<unknown>['object']
 >;
-
-const initLangfuse = ({
-    langfuseSecretKey,
-    langfusePublicKey,
-    langfuseBaseUrl,
-}: {
-    langfuseSecretKey: string;
-    langfusePublicKey: string;
-    langfuseBaseUrl: string;
-}) => {
-    if (langfuse) return;
-    langfuse = new Langfuse({
-        secretKey: langfuseSecretKey,
-        publicKey: langfusePublicKey,
-        baseUrl: langfuseBaseUrl,
-    });
-};
--- monsoft-crm-app/bases/ai/providers/stream-text.provider.ts
+++ monsoft-crm-app/bases/ai/providers/stream-text.provider.ts
@@ -1,20 +1,13 @@
+import { streamText as aiSdkStreamText, Message, smoothStream } from 'ai';
+
 import { Function } from '@errors/types';
 import { Error, Success } from '@errors/utils';
-
-import { getCoreConf } from '@conf/providers/server';
-
-import { v4 as uuidv4 } from 'uuid';
-
-import { streamText as aiSdkStreamText, Message, smoothStream } from 'ai';
-import { createAnthropic } from '@ai-sdk/anthropic';
-
-import { Langfuse } from 'langfuse';
 
 import { AiRequest } from '../schemas';
 
 import { thinkTool } from '../tools/think.tool';
 
-let langfuse: Langfuse | undefined;
+import { aiModelToSdkModel } from '@ai/utils';
 
 export const streamText = (async ({
     prompt,
@@ -31,64 +24,18 @@ export const streamText = (async ({
           messages: Message[];
           modelParams: AiRequest;
       }) => {
-    const traceId = uuidv4();
+    const { data: model, error: modelError } = await aiModelToSdkModel(
+        modelParams.model,
+    );
 
-    // get the core configuration
-    const coreConfWithError = await getCoreConf();
-
-    const { error: coreConfError } = coreConfWithError;
-
-    if (coreConfError !== null) return Error('MISSING_CORE_CONF');
-
-    const { data: coreConf } = coreConfWithError;
-
-    const {
-        anthropicApiKey,
-        langfuseSecretKey,
-        langfusePublicKey,
-        langfuseBaseUrl,
-    } = coreConf;
-
-    if (!langfuse) {
-        initLangfuse({
-            langfuseSecretKey,
-            langfusePublicKey,
-            langfuseBaseUrl,
-        });
-    }
-    const trace = langfuse?.trace({
-        name: modelParams.callerName,
-        id: traceId,
-        metadata: {
-            prompt,
-            messages,
-            modelParams,
-        },
-        sessionId: modelParams.chatSessionId,
-        userId: modelParams.userId,
-        tags: [modelParams.model, modelParams.provider, modelParams.callerName],
-    });
-
-    const anthropic = createAnthropic({
-        apiKey: anthropicApiKey,
-    });
-
-    const model = modelParams.model;
-
-    const generation = trace?.generation({
-        model,
-        input: messages && messages.length > 0 ? messages : prompt,
-        metadata: {
-            activeTools: modelParams.activeTools,
-        },
-    });
+    if (modelError) return Error('FAIL_TO_INFER_SDK_MODEL');
 
     const cleanMessages = messages?.filter(
         (message) => message.content.length > 0,
     );
 
     const { textStream } = aiSdkStreamText({
-        model: anthropic(model),
+        model,
         prompt,
         messages: cleanMessages,
         tools: {
@@ -104,87 +51,6 @@ export const streamText = (async ({
             if (step.finishReason === 'stop') {
                 return;
             }
-
-            trace?.event({
-                name: `event_inner_step_${
-                    step.toolCalls.length
-                        ? step.toolCalls[0].toolName
-                        : 'no_tool'
-                }`,
-                metadata: {
-                    stepType: step.stepType,
-                    finishReason: step.finishReason,
-                    modelId: step.response.modelId,
-                },
-                input: step.request.body,
-                output: step.response.messages,
-            });
-
-            trace?.generation({
-                name: `inner_step_call_${
-                    step.toolCalls.length
-                        ? step.toolCalls[0].toolName
-                        : 'no_tool'
-                }`,
-                model,
-                input: step.request.body,
-                output: step.response.messages,
-                usage: {
-                    input: step.usage.promptTokens,
-                    output: step.usage.completionTokens,
-                    total: step.usage.totalTokens,
-                },
-                metadata: {
-                    stepType: step.stepType,
-                    finishReason: step.finishReason,
-                },
-            });
-        },
-
-        onFinish: async (result) => {
-            generation?.end({
-                input: messages && messages.length > 0 ? messages : prompt,
-                output: result.text,
-                usage: {
-                    input: result.usage.promptTokens,
-                    output: result.usage.completionTokens,
-                    total: result.usage.totalTokens,
-                },
-                metadata: {
-                    finishReason: result.finishReason,
-                    steps: result.steps.map((step) => ({
-                        type: step.stepType,
-                        finishReason: step.finishReason,
-                        input: step.request.body,
-                        output: step.text,
-                    })),
-                },
-            });
-            await langfuse?.flushAsync();
-        },
-
-        onError: async (error) => {
-            trace?.event({
-                name: 'error',
-                metadata: {
-                    error: error.error,
-                },
-            });
-
-            trace?.update({
-                metadata: {
-                    error: error.error,
-                },
-                tags: ['error'],
-            });
-
-            generation?.end({
-                metadata: {
-                    error: error.error,
-                },
-            });
-
-            await langfuse?.flushAsync();
         },
     });
 
@@ -195,20 +61,3 @@ export const streamText = (async ({
     | { prompt?: undefined; messages: Message[]; modelParams: AiRequest },
     ReadableStreamDefaultReader<string>
 >;
-
-const initLangfuse = ({
-    langfuseSecretKey,
-    langfusePublicKey,
-    langfuseBaseUrl,
-}: {
-    langfuseSecretKey: string;
-    langfusePublicKey: string;
-    langfuseBaseUrl: string;
-}) => {
-    if (langfuse) return;
-    langfuse = new Langfuse({
-        secretKey: langfuseSecretKey,
-        publicKey: langfusePublicKey,
-        baseUrl: langfuseBaseUrl,
-    });
-};
