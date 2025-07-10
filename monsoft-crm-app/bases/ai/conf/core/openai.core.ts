4754f4bf5b68b7e4ce9961dff0e1d349974a720a
hernan <hernan.fg96x@gmail.com>
2025-07-10 11:40:20 +0200

    openai core conf col openai-api-key not-null

--- monsoft-crm-app/bases/ai/conf/core/openai.core.ts
+++ monsoft-crm-app/bases/ai/conf/core/openai.core.ts
@@ -2,5 +2,5 @@ import { text } from '@db/sql';
 
 // openai core configuration
 export const openaiCoreConf = {
-    openaiApiKey: text('openai_api_key'),
+    openaiApiKey: text('openai_api_key').notNull(),
 };
