2d7df4eff6531842811af516e994ecd3e35db50c
hernan <hernan.fg96x@gmail.com>
2025-07-10 15:11:54 +0200

    assistant table

--- monsoft-crm-app/app/db/app.tables.ts
+++ monsoft-crm-app/app/db/app.tables.ts
@@ -6,3 +6,4 @@ export * from '../../mods/product/db';
 export * from '../../mods/contact/db';
 export * from '../../mods/contact-channel/db';
 export * from '../../mods/contact-message/db';
+export * from '../../mods/assistant/db';
---
+++ monsoft-crm-app/mods/assistant/db/assistant.table.ts
@@ -0,0 +1,30 @@
+import { table, text, enumType } from '@db/sql';
+
+import tables from '@db/db';
+
+import { assistantToneEnum } from '../enums';
+
+import { aiModelEnum } from '@ai/enums';
+
+export const assistantModel = enumType('assistant_model', aiModelEnum.options);
+
+export const assistantTone = enumType(
+    'assistant_tone',
+    assistantToneEnum.options,
+);
+
+export const assistant = table('assistant', {
+    id: text('id').primaryKey(),
+
+    brandId: text('brand_id')
+        .notNull()
+        .references(() => tables.brand.id, { onDelete: 'cascade' }),
+
+    name: text('name').notNull(),
+
+    model: assistantModel('model').notNull(),
+
+    tone: assistantTone('tone').notNull(),
+
+    prompt: text('prompt').notNull(),
+});
