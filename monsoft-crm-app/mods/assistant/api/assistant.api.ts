fc67be9f6405b3da4dcba73f85f9c6c8d965f89f
hernan <hernan.fg96x@gmail.com>
2025-07-15 15:01:30 +0200

    get-reply-suggesetions query

--- monsoft-crm-app/mods/assistant/api/assistant.api.ts
+++ monsoft-crm-app/mods/assistant/api/assistant.api.ts
@@ -1,6 +1,7 @@
 import { endpoints } from '@api/providers/server';
 
 // queries
+import { getReplySuggestions } from './get-reply-suggestions.query';
 
 // mutations
 import { askAssistant } from './ask-assistant.mutation';
@@ -9,6 +10,7 @@ import { askAssistant } from './ask-assistant.mutation';
 
 export const assistant = endpoints({
     // queries
+    getReplySuggestions,
 
     // mutations
     askAssistant,
---
+++ monsoft-crm-app/mods/assistant/api/get-reply-suggestions.query.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+import { Error, Success } from '@errors/utils';
+import { catchError } from '@errors/utils/catch-error.util';
+
+import { protectedEndpoint } from '@api/providers/server';
+import { queryMutationCallback } from '@api/providers/server/query-mutation-callback.provider';
+
+export const getReplySuggestions = protectedEndpoint
+    .input(
+        z.object({
+            messageId: z.string(),
+        }),
+    )
+    .query(
+        queryMutationCallback(async ({ input: { messageId }, db }) => {
+            const { data: suggestions, error: suggestionsError } =
+                await catchError(
+                    db.query.replySuggestion.findMany({
+                        where: (record, { eq }) =>
+                            eq(record.messageId, messageId),
+                    }),
+                );
+
+            if (suggestionsError) return Error(suggestionsError);
+
+            const response = suggestions.map(({ id, content }) => ({
+                id,
+                content,
+            }));
+
+            return Success(response);
+        }),
+    );
