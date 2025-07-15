52aac287689c221d7a962bd1a5c98f7f2c704a66
hernan <hernan.fg96x@gmail.com>
2025-07-15 14:31:19 +0200

    get-contact-message provider

--- monsoft-crm-app/mods/contact-message/providers/server/index.ts
+++ monsoft-crm-app/mods/contact-message/providers/server/index.ts
@@ -2,6 +2,7 @@ export * from './send-email-to-contact.provider';
 export * from './send-sms-to-contact.provider';
 export * from './send-whatsapp-to-contact.provider';
 
+export * from './get-contact-message.provider';
 export * from './get-contact-messages.provider';
 export * from './get-contact-compressed-chat.provider';
 export * from './ensure-chat-compression.provider';
