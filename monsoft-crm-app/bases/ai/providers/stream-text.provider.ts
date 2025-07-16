5a16fd132073cc2d7ea9059096d4588789c70eb1
hernan <hernan.fg96x@gmail.com>
2025-07-16 16:22:58 +0200

    stream-text provider integrate langfuse

--- monsoft-crm-app/bases/ai/providers/stream-text.provider.ts
+++ monsoft-crm-app/bases/ai/providers/stream-text.provider.ts
@@ -9,21 +9,42 @@ import { thinkTool } from '../tools/think.tool';
 
 import { aiModelToSdkModel } from '@ai/utils';
 
+import { initLangfuseTraceAndGeneration } from './init-langfuse-trace-and-generation.provider';
+
 export const streamText = (async ({ prompt, messages, modelParams, tools }) => {
-    const { data: model, error: modelError } = await aiModelToSdkModel(
-        modelParams.model,
-    );
+    const { model: modelName, callerName } = modelParams;
+
+    const { data: model, error: modelError } =
+        await aiModelToSdkModel(modelName);
 
     if (modelError) return Error('FAIL_TO_INFER_SDK_MODEL');
 
-    const cleanMessages = messages?.filter(
-        (message) => message.content.length > 0,
-    );
+    const promptOrMessages = messages
+        ? {
+              messages: messages.filter(
+                  (message) => message.content.length > 0,
+              ),
+              prompt: undefined,
+          }
+        : { prompt, messages: undefined };
+
+    const { data: traceAndGeneration, error: traceAndGenerationError } =
+        await initLangfuseTraceAndGeneration({
+            modelName,
+            callerName,
+            toolsNames: Object.keys(tools ?? {}),
+            ...promptOrMessages,
+        });
+
+    if (traceAndGenerationError)
+        return Error('FAIL_TO_INIT_LANGFUSE_TRACE_AND_GENERATION');
+
+    const { langfuse, trace, generation } = traceAndGeneration;
 
     const { textStream } = aiSdkStreamText({
         model,
-        prompt,
-        messages: cleanMessages,
+        ...promptOrMessages,
+
         tools: {
             ...tools,
             think: thinkTool,
@@ -36,9 +57,68 @@ export const streamText = (async ({ prompt, messages, modelParams, tools }) => {
         experimental_transform: smoothStream(),
 
         onStepFinish: (step) => {
-            if (step.finishReason === 'stop') {
+            if (step.finishReason === 'stop' || step.toolCalls.length === 0) {
                 return;
             }
+
+            trace.event({
+                name: `event_inner_step_${
+                    step.toolCalls.at(0)?.toolName ?? 'no_tool'
+                }`,
+                metadata: {
+                    stepType: step.stepType,
+                    finishReason: step.finishReason,
+                    modelId: step.response.modelId,
+                },
+                input: step.request.body,
+                output: step.response.messages,
+            });
+        },
+
+        onFinish: async (result) => {
+            generation.end({
+                input: messages && messages.length > 0 ? messages : prompt,
+                output: result.text,
+                usage: {
+                    input: result.usage.promptTokens,
+                    output: result.usage.completionTokens,
+                    total: result.usage.totalTokens,
+                },
+                metadata: {
+                    finishReason: result.finishReason,
+                    steps: result.steps.map((step) => ({
+                        type: step.stepType,
+                        finishReason: step.finishReason,
+                        input: step.request.body,
+                        output: step.text,
+                    })),
+                },
+            });
+            await langfuse.flushAsync();
+        },
+
+        onError: async (error) => {
+            trace.event({
+                name: 'error',
+                metadata: {
+                    error: error.error,
+                },
+            });
+
+            trace.update({
+                metadata: {
+                    error: error.error,
+                },
+                tags: ['error'],
+            });
+
+            generation.end({
+                metadata: {
+                    error: error.error,
+                },
+            });
+
+            await langfuse.flushAsync();
         },
     });
 
