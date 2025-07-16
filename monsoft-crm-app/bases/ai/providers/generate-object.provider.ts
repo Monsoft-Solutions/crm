56ebc28bba961bdb8bf5c3f42eb7364a1c54eb05
hernan <hernan.fg96x@gmail.com>
2025-07-16 16:28:23 +0200

    generate-object provider integrate langfuse

--- monsoft-crm-app/bases/ai/providers/generate-object.provider.ts
+++ monsoft-crm-app/bases/ai/providers/generate-object.provider.ts
@@ -9,6 +9,8 @@ import { AiRequest } from '../schemas';
 
 import { aiModelToSdkModel } from '@ai/utils';
 
+import { initLangfuseTraceAndGeneration } from './init-langfuse-trace-and-generation.provider';
+
 export const generateObject = async <T>({
     prompt,
     messages,
@@ -28,11 +30,34 @@ export const generateObject = async <T>({
           messages: Message[];
       }
 )) => {
-    const { data: model, error: modelError } = await aiModelToSdkModel(
-        modelParams.model,
-    );
+    const { model: modelName, callerName } = modelParams;
+
+    const { data: model, error: modelError } =
+        await aiModelToSdkModel(modelName);
 
     if (modelError) return Error('FAIL_TO_INFER_SDK_MODEL');
+
+    const promptOrMessages = messages
+        ? {
+              messages: messages.filter(
+                  (message) => message.content.length > 0,
+              ),
+              prompt: undefined,
+          }
+        : { prompt, messages: undefined };
+
+    const { data: traceAndGeneration, error: traceAndGenerationError } =
+        await initLangfuseTraceAndGeneration({
+            modelName,
+            callerName,
+            toolsNames: [],
+            ...promptOrMessages,
+        });
+
+    if (traceAndGenerationError)
+        return Error('FAIL_TO_INIT_LANGFUSE_TRACE_AND_GENERATION');
+
+    const { langfuse, generation } = traceAndGeneration;
 
     const { data: result, error } = await catchError(
         aiSdkGenerateObject({
@@ -43,7 +68,31 @@ export const generateObject = async <T>({
         }),
     );
 
-    if (error) return Error('AI_GENERATE_OBJECT_FAILED');
+    if (error) {
+        generation.end({
+            input: prompt,
+            output: `Error: ${String(error)}`,
+            metadata: {
+                error: String(error),
+            },
+        });
+
+        await langfuse.flushAsync();
+
+        return Error('AI_GENERATE_OBJECT_FAILED');
+    }
+
+    generation.end({
+        input: prompt,
+        output: result.object,
+        usage: {
+            input: result.usage.promptTokens,
+            output: result.usage.completionTokens,
+            total: result.usage.totalTokens,
+        },
+    });
+
+    await langfuse.flushAsync();
 
     return Success(result.object);
 };
