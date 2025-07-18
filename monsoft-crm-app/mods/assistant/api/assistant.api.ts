8790d033dba0b77b20611e96f2cbdb4775036b35
hernan <hernan.fg96x@gmail.com>
2025-07-18 15:16:05 +0200

    get-brand-assistants-ids query

--- monsoft-crm-app/mods/assistant/api/assistant.api.ts
+++ monsoft-crm-app/mods/assistant/api/assistant.api.ts
@@ -3,6 +3,7 @@ import { endpoints } from '@api/providers/server';
 // queries
 import { getReplySuggestions } from './get-reply-suggestions.query';
 import { getConversationFacts } from './get-conversation-facts.query';
+import { getBrandAssistantsIds } from './get-brand-assistants-ids.query';
 
 // mutations
 import { askAssistant } from './ask-assistant.mutation';
@@ -15,6 +16,7 @@ export const assistant = endpoints({
     // queries
     getReplySuggestions,
     getConversationFacts,
+    getBrandAssistantsIds,
 
     // mutations
     askAssistant,
