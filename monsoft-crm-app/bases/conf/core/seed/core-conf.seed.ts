75c0cb572d70af8c94b7292bf880ffaf6885ebcb
hernan <hernan.fg96x@gmail.com>
2025-07-21 14:56:05 +0200

    core-conf seed check if existing

--- monsoft-crm-app/bases/conf/core/seed/core-conf.seed.ts
+++ monsoft-crm-app/bases/conf/core/seed/core-conf.seed.ts
@@ -1,3 +1,4 @@
+import { eq } from 'drizzle-orm';
 import { db } from '@db/providers/server/db-client.provider';
 
 import { coreConfTable } from '../db';
@@ -6,5 +7,18 @@ import { coreConfigData } from './constants';
 
 // Core configuration seeds
 export const coreConfSeed = async () => {
+    console.log('seeding core configuration...');
+
+    const existingCoreConfig = await db.query.coreConfTable.findFirst({
+        where: eq(coreConfTable.id, coreConfigData.id),
+    });
+
+    if (existingCoreConfig) {
+        console.log('core configuration already exists');
+        return;
+    }
+
     await db.insert(coreConfTable).values(coreConfigData);
+
+    console.log('core configuration seeded');
 };
